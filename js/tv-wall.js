8d3bc4bf3d4b996839788ac0bedded2c5077391e
paint1024 <paint1024@hotmail.com>
2022-02-18 09:56:26 +0800

    add favicon

--- js/tv-wall.js
+++ js/tv-wall.js
@@ -1,3 +1,9 @@
+/*
+Author  Johann Li
+Source  https://github.com/johannwpli/
+Website https://johann.li/
+*/
+
 let
     /* get radio */
 
@@ -56,27 +62,27 @@ let
 
     tvSrc = 'https://www.youtube-nocookie.com/embed/',
 
-    /* set wall */
+    /* set wall with YouTube Video ID */
 
-    tvSrc0 = tvSrc + '-upyPouRrB8', tvSrc1 = tvSrc + 'w_Ma8oQLmSM', tvSrc2 = tvSrc + 'XWq5kBlakcQ', 
-    tvSrc3 = tvSrc + 'V9KZGs1MtP4', tvSrc4 = tvSrc + 'sPgqEHsONK8', tvSrc5 = tvSrc + 'jNhh-OLzWlE', 
-    tvSrc6 = tvSrc + 'V0I5eglJMRI', tvSrc7 = tvSrc + '9Auq9mYxFEE', tvSrc8 = tvSrc + 'CV5Fooi8YJA',
+    tvSrc0 = '-upyPouRrB8', tvSrc1 = 'w_Ma8oQLmSM', tvSrc2 = 'XWq5kBlakcQ', 
+    tvSrc3 = 'V9KZGs1MtP4', tvSrc4 = 'sPgqEHsONK8', tvSrc5 = 'jNhh-OLzWlE', 
+    tvSrc6 = 'V0I5eglJMRI', tvSrc7 = '9Auq9mYxFEE', tvSrc8 = 'CV5Fooi8YJA',
 
     tvSrcArr0 = [tvSrc0,tvSrc1,tvSrc2,
                  tvSrc3,tvSrc4,tvSrc5,
                  tvSrc6,tvSrc7,tvSrc8],
 
-    tvSrcA = tvSrc + 'wM0g8EoUZ_E', tvSrcB = tvSrc + 'TCnaIE_SAtM', tvSrcC = tvSrc + 'R2iMq5LKXco', 
-    tvSrcD = tvSrc + 'XGEmg3vhrzU', tvSrcE = tvSrc + 'JAzRXylm3M0', tvSrcF = tvSrc + 'FoBfXvlOR6I', 
-    tvSrcG = tvSrc + 'CKjSm5ZeehE', tvSrcH = tvSrc + 'xL0ch83RAK8', tvSrcI = tvSrc + '2mCSYvcfhtc',
+    tvSrcA = 'wM0g8EoUZ_E', tvSrcB = 'TCnaIE_SAtM', tvSrcC = 'R2iMq5LKXco', 
+    tvSrcD = 'XGEmg3vhrzU', tvSrcE = 'JAzRXylm3M0', tvSrcF = 'FoBfXvlOR6I', 
+    tvSrcG = 'CKjSm5ZeehE', tvSrcH = 'xL0ch83RAK8', tvSrcI = '2mCSYvcfhtc',
 
     tvSrcArr1 = [tvSrcA,tvSrcB,tvSrcC,
                  tvSrcD,tvSrcE,tvSrcF,
                  tvSrcG,tvSrcH,tvSrcI],
 
-    tvSrca = tvSrc + 'fHo4cmOembI', tvSrcb = tvSrc + '9zIbGCdWIh4', tvSrcc = tvSrc + 'B1fUWSGvlsU',
-    tvSrcd = tvSrc + 's2RQuTTn7os', tvSrce = tvSrc + 'n-BMA_a8nM4', tvSrcf = tvSrc + 'ZJ6ZUj8R5uQ', 
-    tvSrcg = tvSrc + 'FHL-o1CJOnY', tvSrch = tvSrc + 'FCOtL6RFN4Y', tvSrci = tvSrc + '2bCDBwyEhkc',
+    tvSrca = 'fHo4cmOembI', tvSrcb = '9zIbGCdWIh4', tvSrcc = 'B1fUWSGvlsU',
+    tvSrcd = 's2RQuTTn7os', tvSrce = 'n-BMA_a8nM4', tvSrcf = 'ZJ6ZUj8R5uQ', 
+    tvSrcg = 'FHL-o1CJOnY', tvSrch = 'FCOtL6RFN4Y', tvSrci = '2bCDBwyEhkc',
 
     tvSrcArr2 = [tvSrca,tvSrcb,tvSrcc,
                  tvSrcd,tvSrce,tvSrcf,
@@ -101,7 +107,7 @@ let
             frameborder='${tvBorder}'
             allow='${tvAllow}'
             allowfullscreen='${tvAllowfullscreen}'
-            src='${tvSrcArr[i]}'
+            src='${tvSrc}${tvSrcArr[i]}'
           ></iframe>`)
         }
       )
