cea3c93cc6bd6e48366ad1be8160ac83dd917af7
Johann Li <paint1024@hotmail.com>
2025-09-08 21:00:43 +0800

    update

--- script/idlist.js
+++ script/idlist.js
@@ -18,7 +18,7 @@ let
             // { id: 'V0I5eglJMRI', title: 'RT, Russia', channel: '' }, // blocked by YouTube, https://twitter.com/googleeurope/status/1498572529409179648
             { id: 'YDvsBbKfLPA', title: 'Sky News, United Kingdom', channel: 'https://www.youtube.com/c/SkyNews' },
             { id: 'XH5SyDW29ys', title: 'TaiwanPlus, Taiwan', channel: 'https://www.youtube.com/@TaiwanPlusLive' },
-            { id: 'KK3bo6v7x8U', title: 'TRT, Turkey', channel: 'https://www.youtube.com/c/trtworld' },
+            { id: 'ZCaNE6kI0tk', title: 'TRT, Turkey', channel: 'https://www.youtube.com/c/trtworld' },
             { id: 'rSUjuIM71a8', title: 'WION, India', channel: 'https://www.youtube.com/c/WION' },
         ],
 
